e133a6adfa46954b26d1497bbb636f609b07321e
Edara Avinash <avinashedara199@gmail.com>
2024-07-17 00:11:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            int k = Integer.parseInt(read.readLine().trim());
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            int ans = obj.getPairsCount(nums, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int sum) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        for(int i:arr)
+        {
+            if(map.containsKey(sum-i))
+            {
+                count=count+map.get(sum-i);
+            }
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        return count;
+    }
+}
